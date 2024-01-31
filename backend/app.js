a88a4caf85e857cde186ad65eda6c576eb4b01ef
firasabidli <abidlifiras@bizerte.r-iset.tn>
2024-01-31 16:52:08 +0100

    fix CORS issue

--- backend/app.js
+++ backend/app.js
@@ -15,4 +15,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/Project-MERN',
 
   app.use(express.json());
 
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
 module.exports = app;
