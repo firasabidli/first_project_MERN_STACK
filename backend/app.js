6f27fdc0de5635bd14bb96d625b319e4c1f31391
firasabidli <abidlifiras@bizerte.r-iset.tn>
2024-01-31 16:49:41 +0100

    connect to mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use((req, res) => {
   res.json('Firas abidli using app.js!');
 });
+mongoose.connect('mongodb://127.0.0.1:27017/Project-MERN',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connected successfully to MongoDB !'))
+  .catch(() => console.log('Connection failed to MongoDB !'));
+
+  app.use(express.json());
 
 module.exports = app;
