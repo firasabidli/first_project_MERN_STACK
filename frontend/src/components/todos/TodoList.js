c5c1623c6dc2633b89cc536ec20f3fba5d895b1e
firasabidli <abidlifiras@bizerte.r-iset.tn>
2024-02-10 16:34:18 +0100

    add success and message to the crud Controller

--- frontend/src/components/todos/TodoList.js
+++ frontend/src/components/todos/TodoList.js
@@ -2,23 +2,28 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BsPencilSquare } from "react-icons/bs";
-import { FaTrashAlt } from "react-icons/fa";
+
 
 import CreateTodo from './CreateTodo';
+import DeleteTodo from './DeleteTodo';
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
+    refreshTodos(); 
+  }, []);
+
+  const refreshTodos = () => {
     axios.get('http://localhost:3001/api/todos')
       .then(response => setTodos(response.data))
       .catch(error => console.error('Erreur de chargement des Todos', error));
-  }, []);
+  };
 
   return (
     <div>
-      <h2 className="text-center">Liste des Todos</h2>
-      <CreateTodo />
+      <h2 className="text-center">Todo List</h2>
+      <CreateTodo  refreshTodos={refreshTodos} />
      
       <table className="table table-hover table-bordered text-center mt-4 ">
         <thead>
@@ -30,22 +35,22 @@ const TodoList = () => {
           </tr>
         </thead>
         <tbody>
-          {todos.map((todo, index) => (
-            <tr key={todo._id}>
-              <th scope="row">{index + 1}</th>
-              <td>{todo.title}</td>
-              <td>{todo.description}</td>
-              <td>
+        {todos.map((todo, index) => (
+          <tr key={todo._id}>
+            <th scope="row">{index + 1}</th>
+            <td>{todo.title}</td>
+            <td>{todo.description}</td>
+            <td>
               <button type="button" className="btn btn-warning">
-                  <BsPencilSquare /> 
-                </button>
-                {' '}
-                <button type="button" className="btn btn-danger">
-                  <FaTrashAlt /> 
-                </button>
-              </td>
-            </tr>
-          ))}
+                <BsPencilSquare /> 
+              </button>
+              {' '}
+              <DeleteTodo todoId={todo._id} onDelete={refreshTodos}/>
+              
+            </td>
+          </tr>
+        ))}
+
         </tbody>
       </table>
       
